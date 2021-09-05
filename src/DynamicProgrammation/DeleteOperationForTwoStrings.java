06c59ad20b067ae1c30ef96e5dffca5b5f8b7ecd
Yinn <1499978024@qq.com>
2021-09-05 10:07:35 +0200

    add exercice 2

--- src/DynamicProgrammation/DeleteOperationForTwoStrings.java
+++ src/DynamicProgrammation/DeleteOperationForTwoStrings.java
@@ -1,23 +1,30 @@
 package DynamicProgrammation;
 
+// 583. Delete Operation for Two Strings
+// Given two strings word1 and word2,
+// return the minimum number of steps required to make word1 and word2 the same.
+// In one step, you can delete exactly one character in either string.
+
 public class DeleteOperationForTwoStrings {
     public int minDistance(String word1, String word2) {
-        int len1 = word1.length();
-        int len2 = word2.length();
-
-        if (len1 == 0 && len2 == 0) {
-            return 0;
-        } else if(len1 != 0 && len2 != 0) {
-            if (word1.charAt(0) == word2.charAt(0))
-                return minDistance(word1.substring(1, len1), word2.substring(1, len2));
-            else
-                return 1 + Math.min(
-                        minDistance(word1.substring(1, len1), word2),
-                        minDistance(word1, word2.substring(1, len2))
-                );
-        } else if(len1 == 0) {
-            return len2;
-        } else
-            return len1;
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <=m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
